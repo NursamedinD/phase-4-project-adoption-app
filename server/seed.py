00a78629efea7c93277b3c7fd97548556cb58538
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-01-29 08:57:01 +0300

    added data for seeding

---
+++ server/seed.py
@@ -0,0 +1,41 @@
+from app import app, db
+from models import Parent, Child, ChildParents
+from faker import Faker
+from datetime import datetime
+
+fake = Faker()
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+def seed_data():
+    for i in range (5):
+        parent = Parent(
+            name=fake.name(),
+            username=fake.user_name(),
+            email=fake.email(),
+            password=fake.password()
+        )
+        db.session.add(parent)
+    
+    parents = Parent.query.all()
+    for i in range (5):
+        child = Child(
+            name=fake.name(),
+            age=fake.random_int(min=1, max=18),
+            description=fake.text(),
+            added_by=fake.random_element(parents).id
+        )
+        db.session.add(child)
+
+    children = Child.query.all()
+    for child in children:
+        adoption = ChildParents(
+            parent_id=fake.random_element(parents).id,
+            child_id = child.id,
+            adoption_date=fake.date_this_decade()
+        )
+        db.session.add(adoption)
+
+    db.session.commit()

8301f5bb8c5855283f713f125b6cf3e79a6fa5f5
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-02-01 14:41:39 +0300

    added code to Signup

---
+++ my-app/components/Signup.js
@@ -0,0 +1,31 @@
+import { Formik, Form, Field, ErrorMessage} from "formik";
+import * as Yup from "yup";
+import { registerUser } from "../api";
+import { useNavigate } from "react-router-dom";
+
+function Signup () {
+    const navigate = useNavigate();
+
+    return (
+        <Formik
+        initialValues={{ email: "", password: ""}}
+        validationSchema={Yup.object({
+            email: Yup.string().email("Invalid email").required("Required"),
+            password: Yup.string().min(6, "Password too short!").required("Required"),
+        })}
+        onSubmit={(values) => {
+            registerUser(values).then(() => navigate("/"));
+        }}
+        >
+        <Form>
+            <Field name="email" type="email" placeholder="email" />
+            <ErrorMessage name="email" component="div" />
+            <Field name="password" type="password" placeholder="password" />
+            <ErrorMessage name="password" component="div" />
+            <button type="submit">Signup</button>
+        </Form>
+        </Formik>
+    );
+}
+
+export default Signup;

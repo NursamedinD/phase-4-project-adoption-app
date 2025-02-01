9ebee338ab1abcda74761cdb0a045538c26aa1a1
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-02-01 14:09:53 +0300

    added code to Home

--- my-app/components/Home.js
+++ my-app/components/Home.js
@@ -1,2 +1,26 @@
 import { useEffect, useState} from "react";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import { fetchChildren} from "../api";
+
+function Home() {
+    const [children, setChildren] = useState([]);
+
+    useEffect(() => {
+        fetchChildren().then(setChildren);
+    }, [])
+
+    return (
+        <div>
+            <h2>Children Available for Adoption</h2>
+            {childrem.map((child) => (
+                <div key={child.id}>
+                    <h3>{child.name}</h3>
+                    <p>Age: {child.age}</p>
+                    <Link to={`/child/${child.id}`}>View More</Link>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Home

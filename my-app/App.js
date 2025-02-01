daf4429dde32cba24d21fa282605cac6a1f9925c
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-02-01 13:47:49 +0300

    added Private Route component

---
+++ my-app/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import ChildDetail from "./components/ChildDetail";
+import Login from "./components/Login";
+import Signup from "./components/Signup";
+import AdoptedChildren from "./components/AdoptedChildren";
+import PrivateRoute from "./components/PrivateRoute";
+
+
+function App() {
+    return (
+        <Router >
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/child/:id" element={<ChildDetail />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/adopted-children" element={<PrivateRoute><AdoptedChildren /></PrivateRoute>} />
+            </Routes>
+        </Router>
+    )
+}

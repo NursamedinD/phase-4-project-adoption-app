561902137d5074c0d8d34f09ad707cd3eaaee20c
Nursamedin Abdi Dualle <nursamedin.a.dualle@gmail.com>
2025-02-01 14:27:31 +0300

    added ChildDetail code

---
+++ my-app/components/ChildDetail.js
@@ -0,0 +1,26 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { fetchChildById } from "../api"
+import AdoptionForm from "./AdoptedForm";
+
+function ChildDetail() {
+    const { id } = useParams();
+    const [child, setChild] = useState(null)
+
+    useEffect(() => {
+        fetchChildById(id).then(setChild);
+    }, [id]);
+
+    if (!child) return <p>Loading...</p>;
+
+    return (
+        <div>
+        <h2>{child.name}</h2>
+        <p>Age: {child.age}</p>
+        <p>{child.description}</p>
+        <AdoptionForm childId={child.id} />
+        </div>
+    )
+}
+
+export default ChildDetail;
